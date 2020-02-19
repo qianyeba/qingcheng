fb1f02aee9dc0ec0c4f54371417bce2fd817c6e4
qianye <17352921167@163.com>
2020-02-19 20:33:36 +0800

    day01

--- qingcheng_pojo/src/main/java/com/qingcheng/pojo/goods/Brand.java
+++ qingcheng_pojo/src/main/java/com/qingcheng/pojo/goods/Brand.java
@@ -7,11 +7,16 @@ import java.io.Serializable;
 @Table(name = "tb_brand")
 public class Brand implements Serializable {
 
+
     @Id
     private Integer id;
+
     private String name;
+
     private String image;
+
     private String letter;
+
     private Integer seq;
 
     public Integer getId() {
--- qingcheng_service_goods/src/main/java/com/qingcheng/dao/BrandMapper.java
+++ qingcheng_service_goods/src/main/java/com/qingcheng/dao/BrandMapper.java
@@ -4,4 +4,5 @@ import com.qingcheng.pojo.goods.Brand;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface BrandMapper extends Mapper<Brand> {
+
 }
